ea95b9ab9d5f1d863c2cd88ad2ae4a36e6a925ad
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-03-04 20:34:16 +0200

    add category widget

--- lib/ui/event_details/home_page/home_page.dart
+++ lib/ui/event_details/home_page/home_page.dart
@@ -1,44 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:local_events_app/app_state.dart';
+import 'package:local_events_app/model/category.dart';
+import 'package:provider/provider.dart';
 
-import 'home_page_background.dart';
 import 'package:local_events_app/styleguide.dart';
+
+import 'package:local_events_app/ui/event_details/home_page/category_widget.dart';
+import 'package:local_events_app/ui/event_details/home_page/home_page_background.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          HomePageBackground(
-            screenHeight: MediaQuery.of(context).size.height,
-          ),
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          "LOCAL EVENTS",
-                          style: fadedTextStyle,
-                        ),
-                        Spacer(),
-                        Icon(
-                          Icons.person_outline,
-                          color: Color(0x99FFFFFF),
-                          size: 30,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+      body: ChangeNotifierProvider(
+        create: (_) => AppState(),
+        child: Stack(
+          children: <Widget>[
+            HomePageBackground(
+              screenHeight: MediaQuery.of(context).size.height,
             ),
-          )
-        ],
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "LOCAL EVENTS",
+                            style: fadedTextStyle,
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.person_outline,
+                            color: Color(0x99FFFFFF),
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      child: Text(
+                        "What's Up",
+                        style: whiteHeadingTextStyle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 24.0),
+                      child: Consumer<AppState>(
+                        builder: (context, appState, _) =>
+                            SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: <Widget>[
+                              for (final category in categories)
+                                CategoryWidget(category: category)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
